789bf03415b9546aec9f2b69e00a5713381a4615
BELMONTE, FACUNDO DAMIAN <facundobelmonte11@gmail.com>
2026-08-20 10:35:22 -0300

    TP!: Mi Primera Aplicación Web

---
+++ tp1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	staticDir := "./static"
+
+	fileServer := http.FileServer(http.Dir(staticDir))
+
+	http.Handle("/", fileServer)
+
+	port := ":8080"
+	fmt.Printf("Iniciando aplicación: Gestor de Trabajos Practicos\n")
+	fmt.Printf("Servidor escuchando en http://localhost%s\n", port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Printf("Error al iniciar el servidor: %s\n", err)
+	}
+}
